d41f4271807f1df31a7c9a3583841e64787e8d8b
Afiq Sazali <afiqsazali@gmail.com>
2021-11-02 23:06:40 +0800

    commenting new

--- OS Project G2 C-Look/main.cpp
+++ OS Project G2 C-Look/main.cpp
@@ -14,71 +14,90 @@ using namespace std;
 
 int main(){
 
-    int i,j,k,n,m,sum = 0,x,y,h;
+    int i,k,num_req,disk_size,sum = 0,head;
 
-    cout << "Enter the size of disk\n";
-    cin >> m;
+    cout << "Enter the size of disk\n";				//prompt the user to enter the disk size
+    cin >> disk_size;
 
-    cout << "Enter number of requests\n";
-    cin >> n;
+    cout << "Enter number of requests\n";			//prompt the user to enter the number of page requests
+    cin >> num_req;
 
-    cout << "Enter the requests\n";
-    vector <int> a(n),l;
+    cout << "Enter the requests\n";					//prompt the user to enter the values of those total page request given
+    vector <int> a(num_req),l;							//then will create vector of array a and 1
 
-    for(i=0;i<n;i++){
+    for(i=0;i<num_req;i++){								//for loop will enter the value of the requests into the array until the maximum given number of request
 
     	cin >> a[i];
 
     }
 
-    for(i=0;i<n;i++){
+    for(i=0;i<num_req;i++) {
 
-    	if(a[i]>m){
+    	cout << a[i] << " \n";
 
-    		cout << "Error, Unknown position\n";
+    }
+
+    for(i=0;i<num_req;i++){								//this loop will check if the given values of the request are valid, if the values are above the size of disk
+    												//then it will prompt the user that is it an invalid value and exit the program
+    	if(a[i]>disk_size || a[i]<0){
+
+    		cout << "Error, " << a[i] << " is Out Of Bounds\n";
             return 0;
 
     	}
 
     }
 
-    cout << "Enter the head position\n";
-    cin >> h;
+    cout << "Enter the head position\n";			//the head position will be the beginning of the algorithm operation
+    cin >> head;									//the user will be prompted to enter the value of the head
 
-    a.push_back(h);
-    sort(a.begin(),a.end());
+    while(1) {										//this is check to ensure the head position is inside the disk size
 
-    for(i=0;i<a.size();i++){
+    	if (head > disk_size || head < 0) {
 
-    	if(h==a[i])
+    		cout << "Head out of Bounds, Enter head again: ";
+    		cin >> head;
+
+    	}
+    	else
+    		break;
+
+    }
+
+    a.push_back(head);								//push_back function will push the head to the far end of the array vector
+    sort(a.begin(),a.end());						//sort function will sort the values of the array into ascending order
+
+    for (i=0;i<a.size();i++){						//for loop will count the number of jumps from the beginning of array a to the head
+
+    	if (head==a[i])
             break;
 
     }
 
-    k=i;
-    if(k<n/2){
+    k=i;											//
+    if(k<num_req/2){
 
-    	for(i=k;i<a.size();i++){
-
-            l.push_back(a[i]);
-
-        }
-
-        for(i=0;i<k;i++){
+    	for (i=k;i<a.size();i++){
 
             l.push_back(a[i]);
 
         }
 
-    } else{
-
-    	for(i=k;i>=0;i--){
+        for (i=0;i<k;i++){
 
             l.push_back(a[i]);
 
         }
 
-        for(i=a.size()-1;i>k;i--){
+    } else {
+
+    	for (i=k;i>=0;i--){
+
+            l.push_back(a[i]);
+
+        }
+
+        for (i=a.size()-1;i>k;i--){
 
             l.push_back(a[i]);
 
@@ -86,20 +105,20 @@ int main(){
 
     }
 
-    int temp=l[0];
+    int temp = l[0];
 
-    cout<<temp;
+    cout << temp;
 
-    for(i=1;i<l.size();i++){
+    for (i=1;i<l.size();i++){
 
-    	cout<<" -> "<<l[i]<<' ';
+    	cout << " -> " << l[i] << ' ';
         sum += abs(l[i]-temp);
         temp = a[i];
 
     }
 
-    cout<<'\n';
-    cout<<"Total head movements = "<<sum<<'\n';
+    cout << '\n';
+    cout << "Total head movements = " << sum << '\n';
     return 0;
 
 }
